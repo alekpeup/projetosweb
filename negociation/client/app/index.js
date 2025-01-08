900ddc730c086a731f99f18188d4f0b375fc321e
alekpeup <alekpereira700@gmail.com>
2025-01-08 16:11:35 -0300

    Add files via upload

---
+++ negociation/client/app/index.js
@@ -0,0 +1,13 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade')
+]
+
+var tbody = doument.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('click', function(event){
+    alert('shit');
+})
+
+
